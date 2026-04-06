47f5b5150c67d41b1869828d5aa78b3ba0b6d2c0
nomi135 <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>
2026-04-07 03:38:21 +0500

    CouponAPI CRUD

--- Mango.Services.CouponAPI/Program.cs
+++ Mango.Services.CouponAPI/Program.cs
@@ -1,4 +1,5 @@
 
+using AutoMapper;
 using Mango.Services.CouponAPI.Data;
 using Microsoft.EntityFrameworkCore;
 
@@ -16,10 +17,25 @@ namespace Mango.Services.CouponAPI
                 options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
             });
 
+            IMapper mapper = MappingConfig.RegisterMaps().CreateMapper();
+            builder.Services.AddSingleton(mapper);
+            builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies()); //for using automapper using dependency injection
+
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+
+            builder.Services.AddCors(options =>
+            {
+                options.AddPolicy("AllowAll", policy =>
+                {
+                    policy.AllowAnyOrigin()
+                          .AllowAnyMethod()
+                          .AllowAnyHeader();
+                });
+            });
+
 
             var app = builder.Build();
 
@@ -32,12 +48,29 @@ namespace Mango.Services.CouponAPI
 
             app.UseHttpsRedirection();
 
-            app.UseAuthorization();
+            // CORS must come before Authorization
+            app.UseCors("AllowAll");
 
+            app.UseAuthorization();
 
             app.MapControllers();
 
+            ApplyMigration();
+
             app.Run();
+
+            void ApplyMigration()
+            {
+                using (var scope = app.Services.CreateScope())
+                {
+                    var _db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+
+                    if(_db.Database.GetMigrations().Count() > 0)
+                    {
+                        _db.Database.Migrate();
+                    }
+                }
+            }
         }
     }
 }
